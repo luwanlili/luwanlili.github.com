0a57d0bdf421eb6962790cd790f4f6a654bfd65d
luwanlili <luwanlili@yeah.net>
2012-12-09 14:29:35 +0800

    namekkka33

--- js/data.js
+++ js/data.js
@@ -3,21 +3,27 @@ define({
 	title:'停住,思考使人进步',
     brand: '笃·容',
 	home_page:'首页',
-	link1:'C++',
-	link2:'JavaScript',
-	link3:'HTML5',
+	link1:'C',
+	link2:'C++',
+	link3:'PHP',
 	link4:'哲学',
 	link5:'退出',
 	
 	home_page_href:"/",
-	link1_href:"/cpp/",
-	link2_href:"/JavaScript/",
-	link3_href:"/HTML5/",
+	link1_href:"/c/",
+	link2_href:"/cpp/",
+	link3_href:"/php/",
 	
-	dropdown1:"经济学 <b class='caret'></b>",
-	dropdown2:"社会学 <b class='caret'></b>",
+	dropdown1:"Web <b class='caret'></b>",
+	dropdown2:"经济学 <b class='caret'></b>",
 	
-	dropdown_menu1:"<li><a href='#'>宏观经济学</a></li>"+
+	dropdown_menu1:"<li><a href='/html/'>HTML</a></li>"+
+					"<li><a href='css'>CSS</a></li>"+
+					"<li><a href='javascript'>JavaScript</a></li>"+
+					"<li><a href='jquery'>jQuery</a></li>"+
+					"<li><a href='nodejs'>Node.js</a></li>",
+					
+	dropdown_menu2:"<li><a href='#'>宏观经济学</a></li>"+
 					"<li><a href='#'>微观经济学</a></li>"+
 					"<li><a href='#'>金融经济学</a></li>"+
 					"<li><a href='#'>产业组织</a></li>"+
@@ -30,21 +36,7 @@ define({
 					"<li><a href='#'>经济思想史</a></li>"+
 					"<li><a href='#'>农业与资源经济学</a></li>"+
 					"<li><a href='#'>大师与经典</a></li>"+
-					"<li><a href='#'>制度分析</a></li>",
-					
-	dropdown_menu2:"<li><a href='#'>社会思想与理论</a></li>"+
-					"<li><a href='#'>社会研究方法</a></li>"+
-					"<li><a href='#'>社会分层</a></li>"+
-					"<li><a href='#'>经济与组织</a></li>"+
-					"<li><a href='#'>发展社会学</a></li>"+
-					"<li><a href='#'>社会心理学</a></li>"+
-					"<li><a href='#'>人口学</a></li>"+
-					"<li><a href='#'>民族学</a></li>"+
-					"<li><a href='#'>人类学</a></li>"+
-					"<li><a href='#'>大师与经典</a></li>",					
+					"<li><a href='#'>制度分析</a></li>",				
 						  
-	placeholder:'搜索',
-	
-	
-    blog: 'http://leoo2sk.cnblogs.com'
+	placeholder:'搜索'
 });
