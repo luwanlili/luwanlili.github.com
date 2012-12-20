72a2eac009c056de77dc7814a5c4e3f991878aae
luwanlili <luwanlili@yeah.net>
2012-12-20 18:16:50 +0800

    taskchange

--- js/lu.js
+++ js/lu.js
@@ -10,5 +10,5 @@ function tagUpdate(){
 	//window.location.reload(); //整个页面刷新
 	//$(".span4").load(location.href + ' .span4>*'); //局部页面刷新,要注意.span4后面的>* (所有后代元素)不可缺少，不然他会把自身添加进来，就出现嵌套了 
 	//jQuery.ajaxSetup ({cache:false}); //清除本地cache
-	$(".span4").load(location.href+"?v="+Math.random());
+	$(".span4").load(location.href+"?v="+Math.random()+' .span4>*'); //局部页面刷新,要注意.span4后面的>* 
 }
