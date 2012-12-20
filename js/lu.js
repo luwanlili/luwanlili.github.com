65c6e49030de3a7438724b416432fb927e90cd8d
luwanlili <luwanlili@yeah.net>
2012-12-20 08:26:17 +0800

    namekkka33

--- js/lu.js
+++ js/lu.js
@@ -7,5 +7,6 @@ $(window).ready(function(){
 
 function startRequest(){
 	//window.location.reload();
-	$(".span4").load(location.href + ' .span4>*');
+	$(".span4").load(location.href + ' .span4>*'); //.span4后面的>* (所有后代元素)不要忘记，不然他会把自身添加进来，就出现嵌套了 
+	jQuery.ajaxSetup ({cache:false});
 }
