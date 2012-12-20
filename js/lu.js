a84e01312b4e0ef71218b86e4df8cbfa7fe3367b
luwanlili <luwanlili@yeah.net>
2012-12-20 18:27:56 +0800

    taskchange

--- js/lu.js
+++ js/lu.js
@@ -2,13 +2,17 @@ $(window).ready(function(){
 	//$("pre").addClass("prettyprint");	//.addClass("prettyprint linenums");
 	//prettyPrint();
 	
-	//setInterval("tagUpdate()",60000);//每分钟刷新一次
+	//setInterval("startRequest()",60000);//每分钟刷新一次
 	tagUpdate();
 });
 
-function tagUpdate(){
+//定时刷新
+function startRequest(){
 	//window.location.reload(); //整个页面刷新
-	//$(".span4").load(location.href + ' .span4>*'); //局部页面刷新,要注意.span4后面的>* (所有后代元素)不可缺少，不然他会把自身添加进来，就出现嵌套了 
-	//jQuery.ajaxSetup ({cache:false}); //清除本地cache
-	$(".span4").load(location.href+"?v="+Math.random()+' .span4>*'); //局部页面刷新,要注意.span4后面的>* 
+	$(".span4").load(location.href + ' .span4>*'); //局部页面刷新,要注意.span4后面的>* (所有后代元素)不可缺少，不然他会把自身添加进来，就出现嵌套了 
+	jQuery.ajaxSetup ({cache:false}); //清除本地cache
+}
+
+function tagUpdate(){
+	$(".span4").load(location.href+"?v="+Math.random()+' .span4>*');
 }
