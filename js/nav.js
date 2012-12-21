967eb92bfecee0040d643c48533bc30a1dbfebe2
luwanlili <luwanlili@yeah.net>
2012-12-21 12:39:39 +0800

    modify a post

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,59 @@
-document.write("");
+document.write("
+			<div class='navbar'>
+			    <div class='navbar-inner'>
+			      <div class='container'>
+			        <a data-target='.nav-collapse' data-toggle='collapse' class='btn btn-navbar'>
+			          <span class='icon-bar'></span>
+			          <span class='icon-bar'></span>
+			          <span class='icon-bar'></span>
+			        </a>
+			        
+			        <div class='nav-collapse'>
+			          <ul class='nav'>
+			            <li id='home'><a href='/'>首页</a></li>
+			            <li id='c'><a href='/c/'>C</a></li>
+			            <li id='cpp'><a href='/cpp/'>C++</a></li>
+			            <li id='php'><a href='/php/'>PHP</a></li>
+			            <li  id='web' class='dropdown'>
+			              <a data-toggle='dropdown' class='dropdown-toggle'>Web <b class='caret'></b></a>
+			              <ul class='dropdown-menu'>
+							<li id='html'><a href='/html/'>HTML</a></li>
+							<li id='css3'><a href='/css3/'>CSS</a></li>
+							<li id='javascript'><a href='/javascript/'>JavaScript</a></li>
+							<li id='jquery'><a href='/jquery/'>jQuery</a></li>
+							<li id='nodejs'><a href='/nodejs/'>Node.js</a></li>
+						  </ul>
+			            </li>
+			          </ul>
+			          <form action='' class='navbar-search pull-left'>
+			            <input type='text' placeholder='搜索' class='search-query span3'>
+			          </form>
+			          <ul class='nav pull-right'>
+			            <li id='math'><a href='/math/'>数学</a></li>
+			            <li class='divider-vertical'></li>
+			            <li  id='' class='dropdown'>
+			              <a data-toggle='dropdown' class='dropdown-toggle'>经济学 <b class='caret'></b></a>
+			              <ul class='dropdown-menu'>
+							<li><a href='#'>宏观经济学</a></li>
+							<li><a href='#'>微观经济学</a></li>
+							<li><a href='#'>金融经济学</a></li>
+							<li><a href='#'>产业组织</a></li>
+							<li><a href='#'>计量经济学</a></li>
+							<li><a href='#'>国际贸易理论</a></li>
+							<li class='divider'></li>
+							<li class='nav-header'>其它经济学</li>
+							<li><a href='#'>发展经济学</a></li>
+							<li><a href='#'>公共经济学</a></li>
+							<li><a href='#'>经济思想史</a></li>
+							<li><a href='#'>农业与资源经济学</a></li>
+							<li><a href='#'>大师与经典</a></li>
+							<li><a href='#'>制度分析</a></li>
+						  </ul>
+			            </li>
+						<!--<li><a id='logout'></a></li>-->
+			          </ul>
+			        </div><!-- /.nav-collapse -->
+			      </div>
+			    </div><!-- /navbar-inner -->
+			  </div>
+");
